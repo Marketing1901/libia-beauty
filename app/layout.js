e849abe1b7ef6864273e956e4a7e2b3648ea8c1c
Marketing1901 <sebasgcampos@gmail.com>
2026-09-13 23:09:43 -0400

    Libia Beauty V4 - Initial app

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './style.css';
+export const metadata={title:'Libia Beauty Salon Rewards',description:'Fidelidad y referidos'};
+export default function RootLayout({children}){return <html lang="es"><body>{children}</body></html>}
